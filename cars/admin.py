7e7c04eb333e19cf20b5b8cb56fdf073466db3d1
tryzediv <tmn.harin.d@gmail.com>
2023-02-15 15:08:30 +0400

    Add cars specifications

--- cars/admin.py
+++ cars/admin.py
@@ -4,4 +4,4 @@ from cars.models import Car
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['name', 'year', 'engine', 'power', 'transmission', 'price']
--- cars/models.py
+++ cars/models.py
@@ -3,7 +3,18 @@ from django.db import models
 
 class Car(models.Model):
     name = models.CharField('Название', max_length=50)
-    price = models.IntegerField('Цена')
-    description = models.TextField('Описание', max_length=50)
+    year = models.IntegerField('Год выпуска')
+    engine = models.FloatField('Объем двигателя', max_length=50, default=1.4)
     power = models.IntegerField('Мощность')
     body = models.CharField('Тип кузова', max_length=50)
+    transmission = models.CharField('Тип коробки передач', max_length=50)
+    price = models.IntegerField('Цена')
+    description = models.TextField('Описание')
+
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
