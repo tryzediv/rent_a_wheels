00ac7d12c9a00d8af6f6a1c3ef853ceb3bde0c1d
tryzediv <tmn.harin.d@gmail.com>
2023-02-15 18:00:13 +0400

    Add cars pages

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from cars.models import Car
 
 
 def cars_list(request):
     cars = Car.objects.all()
     return render(request, 'cars/cars_list.html', {'cars': cars})
+
+
+def car_detail(request, car_id):
+    car = get_object_or_404(Car, id=car_id)
+    return render(request, 'cars/car_detail.html', {'car': car})
+
--- rent_a_wheels/urls.py
+++ rent_a_wheels/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from cars.views import cars_list
+from cars.views import cars_list, car_detail
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', cars_list)
+    path('', cars_list),
+    path('<int:car_id>', car_detail)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
