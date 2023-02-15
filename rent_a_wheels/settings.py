26cf9de4b37ee968902a7f43f2b1e8b701fb7505
tryzediv <tmn.harin.d@gmail.com>
2023-02-15 18:14:07 +0400

    Add index.html

--- rent_a_wheels/settings.py
+++ rent_a_wheels/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'rent_a_wheels.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
