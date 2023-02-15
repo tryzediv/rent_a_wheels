18d6159fb246bdc1fc6efa65f04728a0b3c775aa
tryzediv <tmn.harin.d@gmail.com>
2023-02-15 19:54:05 +0400

    Change form

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from cars.models import Car
 from orders.forms import OrderForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def cars_list(request):
@@ -11,8 +13,16 @@ def cars_list(request):
 def car_detail(request, car_id):
     car = get_object_or_404(Car, id=car_id)
     form = OrderForm(request.POST or None, initial={'car': car})
+
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-    return render(request, 'cars/car_detail.html', {'car': car, 'form': form})
+            url = reverse('car', kwargs={'car_id': car.id})
+            return HttpResponseRedirect(f'{url}?sent=1')
+
+    return render(request, 'cars/car_detail.html', {
+        'car': car,
+        'form': form,
+        'sent': request.GET.get('sent')
+    })
 
