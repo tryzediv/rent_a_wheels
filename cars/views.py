98f2a0a8cec974ae816dd8852318e510185c6211
tryzediv <tmn.harin.d@gmail.com>
2023-02-15 15:29:55 +0400

    Add first site's page

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+
+def cars_list(request):
+    cars = Car.objects.all()
+    return render(request, 'cars/cars_list.html', {'cars': cars})
