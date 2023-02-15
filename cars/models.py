489f2c71de7724db4f94a785ff9a02b84ee15b13
tryzediv <tmn.harin.d@gmail.com>
2023-02-15 14:26:38 +0400

    Create 'car' model

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField('Название', max_length=50)
+    price = models.IntegerField('Цена')
+    description = models.TextField('Описание', max_length=50)
+    power = models.IntegerField('Мощность')
+    body = models.CharField('Тип кузова', max_length=50)
